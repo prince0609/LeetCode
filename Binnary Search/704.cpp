25d61cc4e1399792eea48eff11bdbeb7b155615e
Prince Senjaliya <princepatel692005@gmail.com>
2025-07-12 18:03:15 +0530

    Binary search added

---
+++ Binnary Search/704.cpp
@@ -0,0 +1,30 @@
+class Solution {
+
+public:
+
+    int binary_search(vector<int> &arr, int l, int r, int target) {
+
+        if(l>r) {
+            return -1; 
+        }
+
+        int mid = r - ( r - l)/2;
+
+        if(arr[mid] == target) {
+            return mid;
+        }
+        else if(arr[mid] > target) {
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+
+        return binary_search(arr, l, r, target);
+    }
+
+    int search(vector<int>& nums, int target) {
+        
+        return binary_search(nums, 0, nums.size() - 1, target);
+    }
+};
